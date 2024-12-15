9d36c35bec9115a35b525d61747c76814615b8a0
Ialalendd <71377056+Ialalendd@users.noreply.github.com>
2024-12-15 19:04:55 +0000

    Create la.js

---
+++ la.js
@@ -0,0 +1,23 @@
+import '../utils/common.js'
+import { getKeyByValue, newAbortSignal, readWallets, sleep, timestampToDate, random, getProxy, sortObjectByKey, generateFormattedString } from '../utils/common.js'
+import axios from "axios"
+import { Table } from 'console-table-printer'
+import { createObjectCsvWriter } from 'csv-writer'
+import cliProgress from 'cli-progress'
+import moment from "moment"
+import { config } from '../user_data/config.js'
+import { cleanByChecker, getCountByChecker, getWalletFromDB, saveWalletToDB } from '../utils/db.js'
+
+const columns = [
+    { name: 'n', color: 'green', alignment: "right" },
+    { name: 'Wallet', color: 'green', alignment: "right" },
+    { name: 'Clusters', color: 'green', alignment: "right" },
+    { name: 'TX Count', alignment: 'right', color: 'cyan' },
+    { name: 'Source chains', alignment: 'right', color: 'cyan' },
+    { name: 'Dest chains', alignment: 'right', color: 'cyan' },
+    { name: 'Contracts', alignment: 'right', color: 'cyan' },
+    { name: 'Days', alignment: 'right', color: 'cyan' },
+    { name: 'Weeks', alignment: 'right', color: 'cyan' },
+    { name: 'Months', alignment: 'right', color: 'cyan' },
+    { name: 'First tx', alignment: 'right', color: 'cyan' },
+    { name: 'Last tx', alignment: 'right', color: 'cyan' },
